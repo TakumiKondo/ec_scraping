b9976a17cf0bf9b3596bac8ba976bcfe20e740a4
Takumi Kondo <s.093.kondo@gmail.com>
2022-05-04 11:12:04 +0900

    「在庫切れ」を通知先から除外する

--- src/main/kotlin/com/demo/scraper/service/ItemService.kt
+++ src/main/kotlin/com/demo/scraper/service/ItemService.kt
@@ -27,10 +27,17 @@ class ItemService {
         return Scraping(url).get()
     }
 
+    // TODO: 品切れに対応し、品切れであれば通知はしない
     private fun toItem(doc: Document): Item {
         val code: String = doc.select("span.fs-c-productNumber__number").first()?.text().toString()
         val name: String = doc.select("span.fs-c-productNameHeading__name").first()?.text().toString()
-        val size = doc.select("span.fs-c-variationCart__variationName__name").map { it.text() }
+        val size = doc.select("span.fs-c-variationCart__variationName__name")
+            .filter {
+                val ele = it.parent()?.select("span.fs-c-variationCart__variationName__stock--outOfStock")
+                !(ele != null && "在庫切れ" == ele.text())
+            }.map {
+                it.text()
+            }
         return Item(code, name, size.stream().collect(Collectors.toList()))
     }
 }
