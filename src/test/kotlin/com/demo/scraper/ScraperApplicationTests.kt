47ce1f4e082d3af8285c1dc4a255a6bdd22d2940
Takumi Kondo <s.093.kondo@gmail.com>
2022-05-27 08:49:01 +0900

    テスト失敗させる

--- src/test/kotlin/com/demo/scraper/ScraperApplicationTests.kt
+++ src/test/kotlin/com/demo/scraper/ScraperApplicationTests.kt
@@ -1,12 +1,14 @@
 package com.demo.scraper
 
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class ScraperApplicationTests {
 
-//	@Test
-//	fun contextLoads() {
-//	}
+	@Test
+	fun contextLoads() {
+	}
 
 }
